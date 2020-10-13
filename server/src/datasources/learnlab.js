15fa724bd7a62a866a2782d056c51a2c7f62e76a
Jessie Fu <jessie.fu@uwaterloo.ca>
2020-10-12 21:41:31 -0400

    be skeleton

---
+++ server/src/datasources/learnlab.js
@@ -0,0 +1,12 @@
+const { SQLDataSource } = require('datasource-sql');
+
+const MINUTE = 60;
+
+class LearnlabDB extends SQLDataSource {
+    async getClassroom(id) {
+        const res = await this.db.select("*").from("classroom").where({ id }).cache(MINUTE); // default value for cache: 5sec
+        return res.data;
+    }
+}
+
+module.exports = LearnlabDB;
