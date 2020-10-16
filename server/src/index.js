1fb201cbbc7dbf8ea4b5635f8a71e22b0a3f9561
Jessie Fu <jessie.fu@uwaterloo.ca>
2020-10-15 20:38:58 -0400

    dev improvements

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const LearnlabDB = require('./datasources/learnlab');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const LearnlabDB = require("./datasources/learnlab");
 
 const dbConfig = {
     client: "pg",
@@ -9,16 +9,16 @@ const dbConfig = {
         host: "learnlab-database-1.csosestc6lcm.ca-central-1.rds.amazonaws.com",
         user: "root",
         password: "<PWD HERE>",
-        database: "learnlab_1"
-    }
-}
+        database: "learnlab_1",
+    },
+};
 
-const db = new LearnlabDB(dbConfig); 
+const db = new LearnlabDB(dbConfig);
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    dataSources: () => ({ db })
+    dataSources: () => ({ db }),
 });
 
 server.listen().then(({ url }) => {
