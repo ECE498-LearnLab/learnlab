a08f3fd87062e3d3ab7578e4511fad6bc67f6b64
Jenny12321 <jennyw12321@gmail.com>
2020-10-16 02:06:18 -0400

    changed db settings

--- engagement_subsystem/src/engagement_analysis/settings.py
+++ engagement_subsystem/src/engagement_analysis/settings.py
@@ -81,16 +81,16 @@ WSGI_APPLICATION = 'engagement_analysis.wsgi.application'
 
 DATABASES = {
     "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "learnlab_local",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
+    },
+    "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    },
-    "postgres": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "engagement_subsystem",
-        "USER": "learnlab",
-        "PASSWORD": "learnlab",
-        "HOST": "localhost",
-        "PORT": "54321",
     }
 }
 
