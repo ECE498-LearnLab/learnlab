9fae1a8683b256e8eec6c299efb910422fb76fb9
Jenny12321 <jennyw12321@gmail.com>
2020-10-20 18:16:34 -0400

    Link consumer with ddd script

--- engagement_subsystem/src/consumer.py
+++ engagement_subsystem/src/consumer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import pika, sys, os
 import codecs
+import pyximport; pyximport.install()
+import ddd_analysis
 
 def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -24,9 +26,13 @@ def main():
 
         # decode base64 string to image
         frame = codecs.decode(base64EncodedString.strip(),'base64')
+
         with open('./frame.jpg','wb') as f:
             # save image to frame.jpg in current directory
             f.write(frame)
+
+        analyze = ddd_analysis.DddAnalysis(frame)
+        analyze.run()
 
     channel.basic_consume(queue='frames', on_message_callback=callback, auto_ack=True)
 
