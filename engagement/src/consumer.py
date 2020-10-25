7a674053c5271a194024ab7011aaf7b3caa560d7
muskanaul <mk2aulak@edu.uwaterloo.ca>
2020-10-25 19:01:06 -0400

    add frame batching in frontend

--- engagement/src/consumer.py
+++ engagement/src/consumer.py
@@ -3,12 +3,14 @@ import pika, sys, os
 import codecs
 import pyximport; pyximport.install()
 import ddd_analysis
+import json
+import base64
 
 def main():
-    # change host to localhost if running locally
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
+    global x
+    x = 0
     # we need to make sure the queue exists, if it doesn't exist create it
     # durable means the queue will survive a broker restart
     channel.queue_declare(queue='frames', durable=True)
@@ -17,22 +19,31 @@ def main():
     # this callback function will be called
     def callback(ch, method, properties, body):
         print(" [x] Received Frame")
+        print(body)
+        global x
+
+        batchedData = json.loads(body)
+        base64Frames = []
+
+        for frame in batchedData['frames']:
+            x += 1
+
+            # strip the MIME message from frame string
+            base64EncodedString = frame.encode('utf-8').partition(b",")[2]
+
+            # add padding to base64 if needed (this will append '=' to the end if the string length is not a multiple of 4)
+            pad = len(base64EncodedString)%4
+            base64EncodedString += b"="*pad
+            base64Frames.append(base64EncodedString)
+
+            # # decode base64 string to image
+            # frameImage = codecs.decode(base64EncodedString.strip(),'base64')
+            
+            # with open('./frame-(%d).jpg' % x,'wb') as f:
+            #     # save image to frame.jpg in current directory
+            #     f.write(frameImage)
         
-        # strip the MIME message from body
-        base64EncodedString = body.partition(b",")[2]
-
-        # add padding to base64 if needed (this will append '=' to the end if the string length is not a multiple of 4)
-        pad = len(base64EncodedString)%4
-        base64EncodedString += b"="*pad
-
-        # # decode base64 string to image
-        # frame = codecs.decode(base64EncodedString.strip(),'base64')
-        #
-        # with open('./frame.jpg','wb') as f:
-        #     # save image to frame.jpg in current directory
-        #     f.write(frame)
-
-        analyze = ddd_analysis.DddAnalysis(base64EncodedString)
+        analyze = ddd_analysis.DddAnalysis(base64Frames)
         analyze.run()
 
     channel.basic_consume(queue='frames', on_message_callback=callback, auto_ack=True)
