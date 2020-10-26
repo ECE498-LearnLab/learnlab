a8b897936c6e48fa422760aa5da54aef48e33e66
celine-z <celinezhang_@hotmail.com>
2020-10-25 23:05:35 -0400

    Setup db migrations

---
+++ server/migrations/20201025224001_create_users_table.ts
@@ -0,0 +1,23 @@
+import * as Knex from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id');
+    table.string('first_name').notNullable();
+    table.string('middle_name');
+    table.string('last_name').notNullable();
+    table.string('role').notNullable();
+    table.string('phone_number');
+    table.string('email');
+    table.timestamp('last_login');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("users")
+}
+
