b997de5cbfbdd7a6a7f9ddca1c9a04d1c72541e9
muskanaul <mk2aulak@edu.uwaterloo.ca>
2020-10-26 22:30:26 -0400

    add twilio hooks

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainLayout from './layout/MainLayout';
-import * as serviceWorker from './serviceWorker';
+
+import { CssBaseline } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
+import App from './videochat/App';
+import AppStateProvider, { useAppState } from './videochat/state';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import ErrorDialog from './videochat/components/ErrorDialog/ErrorDialog';
+import LoginPage from './videochat/components/LoginPage/LoginPage';
+import PrivateRoute from './videochat/components/PrivateRoute/PrivateRoute';
+import theme from './videochat/theme';
+import './videochat/types';
+import { VideoProvider } from './videochat/components/VideoProvider';
+import useConnectionOptions from './videochat/utils/useConnectionOptions/useConnectionOptions';
+import UnsupportedBrowserWarning from './videochat/components/UnsupportedBrowserWarning/UnsupportedBrowserWarning';
+
+const VideoApp = () => {
+  const { error, setError } = useAppState();
+  const connectionOptions = useConnectionOptions();
+
+  return (
+    <UnsupportedBrowserWarning>
+      <VideoProvider options={connectionOptions} onError={setError}>
+        <ErrorDialog dismissError={() => setError(null)} error={error} />
+        <App />
+      </VideoProvider>
+    </UnsupportedBrowserWarning>
+  );
+};
 
 ReactDOM.render(
-    <MainLayout />
-  ,
+  <MuiThemeProvider theme={theme}>
+    <CssBaseline />
+    <Router>
+      <AppStateProvider>
+        <Switch>
+          <PrivateRoute exact path="/">
+            <VideoApp />
+          </PrivateRoute>
+          <PrivateRoute path="/room/:URLRoomName">
+            <VideoApp />
+          </PrivateRoute>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </AppStateProvider>
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
