725c6c1823cbfede1e87172d41882be2fa37553b
Jessie Fu <jessie.fu@uwaterloo.ca>
2020-11-15 22:52:14 -0500

    Revert "classroom API and resolver"
    This reverts commit d47ae55a8ad552d3f2c044dd2781e5c15e068c31.

--- server/src/resolvers/classrooms.ts
+++ server/src/resolvers/classrooms.ts
@@ -1,32 +1,31 @@
 import { IDataSource } from "..";
-import {
-    Classroom, ClassroomDetails, CreateClassroomResponse,
-    MutationAddStudentsToClassroomArgs, MutationCreateClassroomArgs,
-    QueryClassroomArgs, QueryClassroomDetailsArgs, Resolvers, Response
-} from "../generated/graphql";
+import { ClassroomDetails, Resolvers, Role } from "../generated/graphql";
 
+const fakeClassroomData = [
+    {
+        id: '1',
+        name: 'BIO-101',
+        subject: 'Biology',
+        description: 'Learn about plants.'
+    },
+    {
+        id: '2',
+        name: 'CLAS-102',
+        subject: 'Mythology',
+        description: 'Classical mythology to bore the mind.'
+    }
+];
 
 const classroomResolver: Resolvers = {
     Query: {
-        classroom: async (_, { id }: QueryClassroomArgs, { dataSources }: { dataSources: IDataSource })
-        : Promise<Classroom> => {
-            return await dataSources.db.classroomAPI().getClassroom(id);
-        },
-        classroomDetails: async (_, args: QueryClassroomDetailsArgs, { dataSources }: { dataSources: IDataSource })
-        : Promise<ClassroomDetails> => {
-            return await dataSources.db.classroomAPI().getClassDetails(args);
+        classroom: (_, { id }) => fakeClassroomData.find(c => c.id === id),
+        classroomDetails: async (_, { id, role }: { id: string, role: Role }, { dataSources }: { dataSources: IDataSource }): Promise<ClassroomDetails> => {
+            return await dataSources.db.getClassDetails(id, role);
         },
     },
     Mutation: {
-        createClassroom: async (_, args: MutationCreateClassroomArgs, { dataSources }: { dataSources: IDataSource })
-        : Promise<CreateClassroomResponse> => {
-            return await dataSources.db.classroomAPI().createClassroom(args);
-        },
-        addStudentsToClassroom: async (_, args: MutationAddStudentsToClassroomArgs,
-            { dataSources }: { dataSources: IDataSource }): Promise<Response> => {
-            return await dataSources.db.classroomAPI().addStudentsToClassroom(args);
-        }
+
     }
-};
+}
 
 export default classroomResolver;
