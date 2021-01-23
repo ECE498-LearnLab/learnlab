a6049aa9a27fe5a22570a19963533822c543aac0
muskanaul <mk2aulak@edu.uwaterloo.ca>
2021-01-23 13:13:17 -0500

    delete unneccesary ui ki files

--- web/src/components/cleanui/layout/TopBar/index.js
+++ web/src/components/cleanui/layout/TopBar/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
-import Search from './Search'
 import LanguageSwitcher from './LanguageSwitcher'
-import Actions from './Actions'
-import UserMenu from './UserMenu'
 import style from './style.module.scss'
+import UserMenu from './UserMenu'
 
 const TopBar = () => {
   return (
     <div className={style.topbar}>
-      <div className="mr-auto">
-        <Search />
-      </div>
       <div className="mr-4 d-none d-sm-block">
         <LanguageSwitcher />
-      </div>
-      <div className="mr-4 d-none d-sm-block">
-        <Actions />
       </div>
       <div className="">
         <UserMenu />
