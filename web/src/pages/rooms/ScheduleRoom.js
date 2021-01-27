8bf2f6c927919be57d22f47dda7898ced6510b40
celine-z <celinezhang_@hotmail.com>
2021-01-27 01:48:39 -0500

    Invite participants to room

--- web/src/pages/rooms/ScheduleRoom.js
+++ web/src/pages/rooms/ScheduleRoom.js
@@ -1,15 +1,13 @@
 import ACL from 'components/cleanui/system/ACL'
 import moment from 'moment'
 import { DatePicker, Form, Input, Modal, notification } from 'antd'
-import { apolloClient } from 'index'
 import React, { useState } from 'react'
-import { gql } from '@apollo/client'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import { injectIntl } from 'react-intl'
 import { Button } from 'reactstrap'
 
 const ScheduleRoom = ({ intl, onSuccess }) => {
   // to:do once global class_id selection is done, change hardcoded class_id value
-  // and add invite participants section to invite students who are taking the class
   const CREATE_ROOM = gql`
     mutation createRoom($class_id: ID!, $name: String!, $start_time: Date, $end_time: Date) {
       createRoom(class_id: $class_id, name: $name, start_time: $start_time, end_time: $end_time) {
@@ -20,11 +18,82 @@ const ScheduleRoom = ({ intl, onSuccess }) => {
       }
     }
   `
+
+  // to:do once global class_id selection is done, change hardcoded class_id value
+  const GET_CLASSROOM = gql`
+    query getClassDetails {
+      classroomDetails(id: "2", role: INSTRUCTOR) {
+        students {
+          id
+          first_name
+          last_name
+          email
+        }
+      }
+    }
+  `
+
+  const INVITE_PARTICIPANTS = gql`
+    mutation inviteToRoom($student_ids: [ID!]!, $room_id: ID!) {
+      invite(student_ids: $student_ids, room_id: $room_id) {
+        success
+        message
+      }
+    }
+  `
+
   const { RangePicker } = DatePicker
   const [form] = Form.useForm()
   const [show, setShow] = useState(false)
 
   const toggleShow = () => setShow(!show)
+
+  const onCreateRoomSuccess = res => {
+    if (res.createRoom.success) {
+      const student_ids = data.classroomDetails.students.map(r => r.id)
+      inviteParticipants({
+        variables: {
+          student_ids,
+          room_id: res.createRoom.id,
+        },
+      })
+      onSuccess()
+      notification.success({
+        message: 'Schedule Success',
+        description: 'Room successfully scheduled',
+      })
+    } else {
+      notification.warning({
+        message: 'Schedule Failure',
+        description: 'An error occurred scheduling the room',
+      })
+    }
+  }
+
+  const onCreateRoomError = err => {
+    notification.warning({
+      message: err.code,
+      description: err.message,
+    })
+  }
+
+  const onInviteParticipantsError = err => {
+    notification.warning({
+      message: 'Invite Participants Failure',
+      description: err.message,
+    })
+  }
+
+  const { data } = useQuery(GET_CLASSROOM)
+
+  const [createRoom] = useMutation(CREATE_ROOM, {
+    onCompleted: onCreateRoomSuccess,
+    onError: onCreateRoomError,
+  })
+
+  const [inviteParticipants] = useMutation(INVITE_PARTICIPANTS, {
+    onError: onInviteParticipantsError,
+  })
 
   const onOk = () => {
     form
@@ -34,8 +103,8 @@ const ScheduleRoom = ({ intl, onSuccess }) => {
         onSubmit(values)
         toggleShow()
       })
-      .catch(error => {
-        onSubmitFailed(error)
+      .catch(err => {
+        onSubmitFailed(err)
       })
   }
 
@@ -49,38 +118,15 @@ const ScheduleRoom = ({ intl, onSuccess }) => {
     return current && current < moment().startOf('day')
   }
 
-  const onSubmit = async values => {
-    await apolloClient
-      .mutate({
-        mutation: CREATE_ROOM,
-        variables: {
-          class_id: 2,
-          name: values.name,
-          start_time: values.time[0],
-          end_time: values.time[1],
-        },
-      })
-      .then(result => {
-        if (result.data.createRoom.success) {
-          onSuccess()
-          notification.success({
-            message: 'Schedule Success',
-            description: 'Room successfully scheduled',
-          })
-          return true
-        }
-        notification.warning({
-          message: 'Schedule Failure',
-          description: 'An error occurred scheduling the room',
-        })
-        return false
-      })
-      .catch(error => {
-        notification.warning({
-          message: error.code,
-          description: error.message,
-        })
-      })
+  const onSubmit = values => {
+    createRoom({
+      variables: {
+        class_id: 2,
+        name: values.name,
+        start_time: values.time[0],
+        end_time: values.time[1],
+      },
+    })
   }
 
   const onSubmitFailed = errorInfo => {
