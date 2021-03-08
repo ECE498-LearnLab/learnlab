5878ab25b829e8813b294ad6f782890548ac58ae
muskanaul <mk2aulak@edu.uwaterloo.ca>
2021-03-07 21:45:32 -0500

    fix lint

--- web/src/pages/rooms/EngagementGraph.js
+++ web/src/pages/rooms/EngagementGraph.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState, useCallback } from 'react'
 import { gql, useSubscription } from '@apollo/client'
 import ApexCharts from 'apexcharts'
+import React, { useCallback, useEffect, useState } from 'react'
 import ReactApexChart from 'react-apexcharts'
 
 const EngagementGraph = ({ room_id }) => {
@@ -25,8 +25,7 @@ const EngagementGraph = ({ room_id }) => {
       // prevents data array from getting too large
       if (graphData.length > 100) resizeData()
     },
-    // eslint-disable-next-line
-    [setGraphData, graphData],
+    [setGraphData, graphData, resizeData],
   )
 
   const resizeData = useCallback(() => {
