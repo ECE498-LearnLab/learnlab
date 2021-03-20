989acdb031398a6b1af2ad3a21f04ed70074b52c
muskanaul <mk2aulak@edu.uwaterloo.ca>
2021-03-20 13:40:39 -0400

    aaaah

--- engagement/src/consumerUtils.py
+++ engagement/src/consumerUtils.py
@@ -3,10 +3,16 @@ import base64
 import json
 import os
 import sys
-from io import BytesIO
+import numpy as np
+import matplotlib.pyplot as plt
 from datetime import datetime
+from io import BytesIO
+from os import walk
+
 from graphqlclient import GraphQLClient
 from PIL import Image
+from sklearn import metrics
+from sklearn.metrics import ConfusionMatrixDisplay
 
 from machine_learning.gaze_model import GazeDetector
 
@@ -16,7 +22,7 @@ def demo_frames():
     test_frames = ["me1.png", "me2.png", "me3.png", "me4.png", "me5.png", "me6.png", "me7.png"]
 
     for frame in test_frames:
-        image = Image.open("./" + frame).resize((200,200)) 
+        image = Image.open("./images/" + frame).resize((200,200)) 
         imageString = img_to_base64_str(image)
         base64Frames.append(imageString)
 
@@ -73,3 +79,50 @@ def process_frame(logger, data):
         gaze_angle, gaze_magnitude, classification, score = convolutional_net.predict_gaze()
         # logger.info(gaze_angle, gaze_magnitude, classification, score*10)
         send_to_graphql(logger, data["studentID"], data["roomID"], data["token"], score*10, classification)
+
+def test_frames(logger, data):
+    actual_output = []
+    predicted_output = []
+    test_frames = []
+    real_classification = {
+        "engaged": "ENGAGED",
+        "not_engaged": "NOT ENGAGED",
+        "somewhat_engaged": "SOMEWHAT ENGAGED"
+    }
+
+    # import test data 
+    directories = ["engaged", "not_engaged", "somewhat_engaged"]
+    test_files = []
+    for dirname in directories:
+        files = [(x,dirname) for x in os.listdir("./test_data/"+ dirname + "/")]
+        test_files.extend(files)
+
+    for f in test_files:
+        image = Image.open("./test_data/" + f[1] + "/" + f[0]).resize((200,200)) 
+        imageString = img_to_base64_str(image)
+        test_frames.append((imageString, f[1]))
+    
+    for frame in test_frames:
+        # strip the MIME message from frame string
+        base64EncodedString = frame[0].encode('utf-8').partition(b",")[2]
+
+        # add padding to base64 if needed (this will append '=' to the end if the string length is not a multiple of 4)
+        pad = len(base64EncodedString)%4
+        base64EncodedString += b"="*pad
+
+        # logger.info("Running Predicting Logic")
+        base64_jpg: str = base64EncodedString
+        convolutional_net = GazeDetector(logger=logger, base_64_image=base64_jpg)
+        # logger.info("Running Gaze Prediction")
+        gaze_angle, gaze_magnitude, classification, score = convolutional_net.predict_gaze()
+        predicted_output.append(real_classification[frame[1]])
+        actual_output.append(classification)
+        # logger.info(gaze_angle, gaze_magnitude, classification, score*10)
+        # send_to_graphql(logger, data["studentID"], data["roomID"], data["token"], score*10, classification)
+    
+    display_labels = labels=["ENGAGED", "SOMEWHAT ENGAGED", "NOT ENGAGED"]
+    logger.info("actual output:", actual_output)
+    logger.info("predicted output:", predicted_output)
+    logger.info(metrics.confusion_matrix(actual_output, predicted_output, labels=display_labels))
+    # Printing the precision and recall, among other metrics
+    logger.info(metrics.classification_report(actual_output, predicted_output, labels=display_labels))
